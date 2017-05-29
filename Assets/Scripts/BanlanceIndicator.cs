e99774e0e91a5b330b55f74d2283fc0d559ba5c1
DESKTOP-H30DEJE\zhou <zhou18825436640@sina.com>
2017-05-29 09:48:05 +0800

    初始化

---
+++ Assets/Scripts/BanlanceIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BanlanceIndicator : MonoBehaviour {
+
+    public Text text;
+
+    public void SetText(int balance)
+    {
+        text.text = "Balance:" + balance;
+
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int balance = 50;
+    public int limit = 100;
+    public float updateRate = 2f; // 每隔2秒调用一次
+    public float lastTime = 0f;
+
+    public BanlanceIndicator indicator;
+
+	// Use this for initialization
+	void Start () {
+        Random.InitState(System.Guid.NewGuid().GetHashCode());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float time = Time.time;
+        if (time - lastTime > updateRate)  // 每隔2秒 加一个随机的力，判断
+        {
+            lastTime = time;
+            ForceAdd();
+        }
+
+        if (balance > limit || balance < 0)
+        {
+            Debug.Log("game over");
+        }
+
+        if (Input.GetKeyDown("z"))
+        {
+            balance -= 5;
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            balance += 5;
+        }
+        Debug.Log(balance);
+
+        indicator.SetText(balance);
+
+    }
+
+    private void ForceAdd() {
+        int random = Random.Range(-20, 20);
+        balance += random;
+        Debug.Log(balance);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
