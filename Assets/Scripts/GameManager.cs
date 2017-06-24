c18fddbb07167521d2d0dbf087ecd6db6a98d248
DESKTOP-H30DEJE\zhou <zhou18825436640@sina.com>
2017-06-25 00:23:20 +0800

    补充视频02完成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static int score = 0;
-
+    
 	// Use this for initialization
 	void Start () {
 		
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -11,14 +11,21 @@ public class GameOver : MonoBehaviour {
     public Button button;
     public string lvlName = "Level2";
     public Platformer2DUserControl player;
+    public Button menuButton;
     public GameObject menu;
+    public Button confirmButton;
+    public Toggle muteToggle;
+    public Slider slider;
 
     // Use this for initialization
     private void Awake() // 适合加载资源的时候使用
     {
         animator = GetComponent<Animator>();
-        menu.active = false;
+        menu.SetActive(false);
+      
         button.onClick.AddListener(ButtonDidAction);
+        menuButton.onClick.AddListener(MenuButtonDidAction);
+        confirmButton.onClick.AddListener(ConfirmButtonDidAction);
     }
 
     void Start() {
@@ -27,7 +34,16 @@ public class GameOver : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (muteToggle.isOn == false) // 如果没有勾选，说明不是静音
+        {                            
+            AudioListener.volume = 1f; // 设置为最大音量
+            AudioListener.volume = slider.value; // 
+        }
+        else
+        {
+            AudioListener.volume = 0f;
 
+        }
     }
 
     public void ShowOver() {
@@ -37,11 +53,23 @@ public class GameOver : MonoBehaviour {
     public void ShowWin() {
         animator.SetTrigger("Win");
         player.enabled = false;
+        menuButton.enabled = false;
+        
     }
 
     private void ButtonDidAction()
     {
         SceneManager.LoadScene(lvlName);
         Debug.Log("buttonDidAction");
+    }
+
+    private void MenuButtonDidAction() {
+        menu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void ConfirmButtonDidAction() {
+        menu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
